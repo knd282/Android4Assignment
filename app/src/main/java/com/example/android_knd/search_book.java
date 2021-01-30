f4fe481d97bee4c4d9df9ea4382d45767073759c
Kyaw Nanda <nandak2@uni.coventry.ac.uk>
2021-01-30 15:42:24 +0800

    Add ISBN scanner using zxing libary

--- app/src/main/java/com/example/android_knd/search_book.java
+++ app/src/main/java/com/example/android_knd/search_book.java
@@ -1,38 +1,56 @@
 package com.example.android_knd;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 //for logout and searching books
 public class search_book extends AppCompatActivity {
-    //for book search
+
+//-------------------------------------------------for book search
     EditText tBook;
     Button btnDisplay;
-    //private WebView webView;
-
-    //logout
+//-------------------------------------------------logout
     private Button logout;
+//--------------------------------------------------logout
+//--------------------------------------------------ISBN
+    Button btnISBNum;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(scanResult.getContents() != null){ //if not null
+            String isbncodeR = scanResult.getContents();
+            //get isbn and send to web_v
+            String passURL = isbncodeR;
+            //send data to web activity
+            Intent launchResult = new Intent(search_book.this, web_view.class);
+            launchResult.putExtra("key", passURL);
+            startActivity(launchResult);
+        }else {
+            //if null
+            Toast.makeText(this, "Code isn't found", Toast.LENGTH_LONG).show();
+        }
+    }
+    //--------------------------------------------------ISBN
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_book);
-
-        //logout start
+//--------------------------------------------------logout
         logout = (Button) findViewById(R.id.signOut);
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,11 +58,27 @@ public class search_book extends AppCompatActivity {
                 startActivity(new Intent(search_book.this, MainActivity.class));
             }
         });
-        //logout end
-
-        // for book search start
+//--------------------------------------------------logout
+//--------------------------------------------------for ISBN
+        btnISBNum = findViewById(R.id.btnISBN);
+        btnISBNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //intent integration
+                IntentIntegrator intentIntegrator = new IntentIntegrator(
+                        search_book.this
+                );
+                intentIntegrator.setPrompt("Volume Up to Open Flash");
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Cam.class);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                intentIntegrator.initiateScan();
+            }
+        });
+//--------------------------------------------------for ISBN
+//--------------------------------------------------for book search start
         tBook = findViewById(R.id.txtBook);
-        btnDisplay = findViewById(R.id.btnBook);
+        btnDisplay = findViewById(R.id.btnSearch);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,11 +90,12 @@ public class search_book extends AppCompatActivity {
                     //get data
                     String passURL = txtBook;
                     //send data to web activity
-                    Intent launchResult = new Intent(search_book.this, web_v.class);
+                    Intent launchResult = new Intent(search_book.this, web_view.class);
                     launchResult.putExtra("key", passURL);
                     startActivity(launchResult);
                 }
             }
         });
+//--------------------------------------------------for book search end
     }
 }
