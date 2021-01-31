ffcb59c7cb71584d47a391854ed5c28a121853d6
Kyaw Nanda <nandak2@uni.coventry.ac.uk>
2021-02-01 01:43:30 +0800

    Add to wishlist 50% complete

--- app/src/main/java/com/example/android_knd/web_view.java
+++ app/src/main/java/com/example/android_knd/web_view.java
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class web_view extends AppCompatActivity {
@@ -30,7 +31,24 @@ public class web_view extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(web_view.this, "Clicked", Toast.LENGTH_SHORT).show();
+                //start
+                String passURL = getIntent().getStringExtra("key");
+                //add wishlist to firebase with auto generate ID
+                DatabaseReference myRef = FirebaseDatabase.getInstance()
+                        .getReference().child("Users")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Wishlist");
+                myRef.push().setValue(passURL).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){//show it if complete
+                            Toast.makeText(web_view.this, "Added to wishlist", Toast.LENGTH_SHORT).show();
+                        }else{//just error handler
+                            Toast.makeText(web_view.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                //End send wishlist to firebase
+
             }
         });
 
